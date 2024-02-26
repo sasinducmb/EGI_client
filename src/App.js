450e2bf75c7e411a3f9544299136598ab20c9659
Sasindu Kavishka <141473967+sasinducmb@users.noreply.github.com>
2024-02-26 14:02:12 +0530

    02 26 update

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/account" element={<Account />} />
-              <Route path="/product-details" element={<ProductDetail />} />
+              <Route path="/productDetails/:id" element={<ProductDetail />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
             </Routes>
